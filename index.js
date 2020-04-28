32600668c202a3365ea4547c18ef5d164353184a
Marco Munizaga <marco@marcopolo.io>
2020-04-28 16:34:02 -0700

    touch

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const { translate } = require('moji-translate')
 exports.endpoint = function(request, response) {
-    response.end("Hello world!!!");
+    response.end(translate()"Hello world!!!"));
 }
